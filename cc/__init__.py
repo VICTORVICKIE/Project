d48f9e97515a333a7de807598570dccc6638f8e6
vignesh kumar s <VICTORVICKIE@users.noreply.github.com>
2020-07-21 20:05:12 +0530

    Added Platform check for dev

--- cc/__init__.py
+++ cc/__init__.py
@@ -1,19 +1,29 @@
 from flask import Flask
 import os
 from flask_mysqldb import MySQL
+from platform import system
 
+current_system = system()
 
 
 ########################################    INITIALIZING  APP and DB   ##########################################
+if current_system == 'Linux':
+	app = Flask(__name__)
+	app.config['MYSQL_HOST'] = 'victorvickie.mysql.pythonanywhere-services.com'
+	app.config['MYSQL_USER'] = 'victorvickie'
+	app.config['MYSQL_PASSWORD'] = 'VIGNESHkumar@14'
+	app.config['MYSQL_DB'] = 'victorvickie$CAMPUS_COINS'
 
-app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'VIGNESHkumar14'
-app.config['MYSQL_DB'] = 'CAMPUS_COINS'
+elif current_system == 'Windows':
+	app = Flask(__name__)
+	app.config['MYSQL_HOST'] = 'localhost'
+	app.config['MYSQL_USER'] = 'root'
+	app.config['MYSQL_PASSWORD'] = 'VIGNESHkumar14'
+	app.config['MYSQL_DB'] = 'CAMPUS_COINS'
+
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config.update(SECRET_KEY=os.urandom(24))
 
+app.config.update(SECRET_KEY=os.urandom(24))
 mysql = MySQL(app)
 
 from cc import routes
